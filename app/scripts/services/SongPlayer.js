e13f7d80bf98b893252920582fc87f8b34dc84f2
cmorganNU97 <christopher.morgan@epbfi.com>
2017-10-16 11:28:11 -0400

    WebDev AngularJS Checkpoint 10: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
 
 /**
 * @desc songPlayer Object that contains factory PlaySong methods
@@ -33,6 +33,12 @@
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
+            });
+
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
             });
 
             SongPlayer.currentSong = song;
@@ -75,6 +81,12 @@
 * @type {Object}
 */
         SongPlayer.currentSong = null;
+
+/**
+* @desc Current playback time (in seconds) of currently playing song
+* @type {Number}
+*/
+         SongPlayer.currentTime = null;
 
 /**
 * @function play method for SongPlayer
@@ -125,6 +137,18 @@
         };
 
 /**
+* @function setCurrentTime
+* @desc Set current time (in seconds) of currently playing song
+* @param {Number} time
+*/
+
+      SongPlayer.setCurrentTime = function(time) {
+          if (currentBuzzObject) {
+              currentBuzzObject.setTime(time);
+          }
+      };
+
+/**
 * @function previous method for SongPlayer
 * @desc When previous button is clicked the song prior to the current song is played
 */
@@ -146,5 +170,5 @@
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', ['Fixtures', SongPlayer]);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
